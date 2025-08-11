7800e089e8619c75735dc744b64ea50e65bbb15e
benji_707 <dontjunkthis@gmail.com>
2025-08-11 03:02:33 -0500

    Instructions comment

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+1. Write a function named reverseString that takes a string as input.
+2. The function should:
+○ Return the string itself if it contains only one character (base case).
+○ Use recursion to reverse the string by concatenating the last character
+of the string with the result of the recursive call on the remaining part
+of the string (recursive case).
+3. Test the function with various string inputs to verify its correctness.
+*/
